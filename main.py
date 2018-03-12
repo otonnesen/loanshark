140687905c4bab9de5c5c3a847b06e1def25da29
Oliver <oliver@tonnesen.com>
2018-03-12 11:42:26 -0700

    Changed startup conditions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import json
 from flask import Flask, jsonify
 
@@ -17,4 +18,5 @@ def main():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
