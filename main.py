c141748b3441831d184a28f1f7a83956ef6b2062
Oliver <oliver@tonnesen.com>
2018-03-13 13:16:34 -0700

    Added postgres to main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
+import psycopg2
 import json
 from flask import Flask, jsonify, render_template
 
 filepath = 'static/data.json'
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 app = Flask(__name__)
 
