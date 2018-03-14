d689c8cdf28f5b8eb6e61e9e18f490d5d9c826e7
Oliver <oliver@tonnesen.com>
2018-03-13 21:59:23 -0700

    hmmmm

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 
 cur.execute('DROP TABLE IF EXISTS test;')
-cur.execute('CREATE TABLE test (id SERIAL PRIMARY KEY, name varchar(40), address varchar(40));')
+cur.execute('CREATE SEQUQNCE user_id_seq;')
+cur.execute('CREATE TABLE test (user_id smallint NOT NULL DEFAULT nextval("user_id_seq"), name varchar(40), address varchar(40));')
+cur.execute('ALTER SEQUENCE user_id_seq OWNED BY user.user_id;')
 cur.execute("INSERT INTO test (name, address) VALUES ('Oliver', '6431 Riverstone Drive');")
 cur.close()
 
