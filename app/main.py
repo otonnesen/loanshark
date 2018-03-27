e7098b1b0c85ed350158ede5e78633687f114de3
Oliver Tonnesen <oliver@tonnesen.com>
2018-03-26 20:38:41 -0700

    db has get_debt_data(), get_credit_data(), get_all_transactions(), and transactions template now shows credits and debts

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import json
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, make_response
 
 filepath = 'static/data.json'
 
@@ -24,29 +24,36 @@ def index():
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    return render_template('main.html')
-
-@app.route('/addUser', methods=['POST'])
-def addUser():
-    with conn.cursor() as cur:
-        fName = request.form['first_name']
-        lName = request.form['last_name']
-        cur.execute('SELECT add_user(%s, %s)', (fName, lName))
-        return render_template('success.html')
+    if request.method == 'POST':
+        with conn.cursor() as cur:
+            fName = request.form['first_name']
+            lName = request.form['last_name']
+            cur.execute('SELECT add_user(%s, %s)', (fName, lName))
+            conn.commit()
+            resp = make_response(redirect('/'))
+            if(cur.query == "SELECT add_user('', '')"):
+                return resp
+            resp.set_cookie('userAddSuccess', value='True')
+            return resp
+    resp = make_response(render_template('main.html', userAddSuccess=request.cookies.get('userAddSuccess')))
+    resp.set_cookie('userAddSuccess', value='')
+    return resp
 
 @app.route('/data')
 def getData():
     with conn.cursor(cursor_factory=RealDictCursor) as cur:
-        cur.execute('SELECT * FROM users;')
+        cur.execute('SELECT * FROM users ORDER BY uid;')
         d = cur.fetchall()
         return jsonify(d)
 
 @app.route('/data/<int:id>')
 def getUserData(id):
     with conn.cursor(cursor_factory=RealDictCursor) as cur:
-        cur.execute('SELECT * FROM get_table_data(%d)' % id)
-        d = cur.fetchall()
-        return render_template('transaction_list.html', transaction_list=d)
+        cur.execute('SELECT * FROM get_credit_data(%d)' % id)
+        credit = cur.fetchall()
+        cur.execute('SELECT * FROM get_debt_data(%d)' % id)
+        debt = cur.fetchall()
+        return render_template('transaction_list.html', credList=credit, debtList=debt, credEmpty=len(credit)==0, debtEmpty=len(debt)==0)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
